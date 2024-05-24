034bef56730dc31dfde6678aad86c8582bd51005
mohamadghayoumi <mohamad.aftabi75@gmail.com>
2024-05-24 17:15:32 +0330

    feat:easy

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,7 +1,7 @@
 
 function AboutUs() {
   return (
-    <div className="mb-[344px] mt-72">
+    <div className="mb-[593px] mt-10 px-7">
         <h3 className="text-3xl mb-10">درباره ما</h3>
         <p className="text-xl mb-10">این یک صفحه درباره کتاب های گروه 3 است با نویسندگی ممد</p>
     </div>
